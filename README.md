9444bedf325b9ae5306fcb928702f667f6017f4e
Neil Sengupta <neil96sengupta@gmail.com>
2016-09-01 14:16:08 +0400

    Add ng filters for images and currency

---
+++ README.md
@@ -0,0 +1,4 @@
+The Chic Store
+==============
+Frontend project dislaying an online fashion store dynamically populating
+available items. Written in `Angular 1.2.x` using Semantic UI and Bootstrp.
--- app.js
+++ app.js
@@ -8,33 +8,46 @@
 	var tees = [
 	{
 		brand: "J-Crew",
-		description : "modern oxford styled for the modern man",
-		price: "$80",
+		description : "Modern oxford styled for the modern man",
+		price: "80",
 		availability: false,
+		images:[
+			"images/porter1.jpg",
+			"images/porter2.jpg",
+
+		]
 	},
 	{
 		brand: "Alexander McQueen",
 		description : "Queensy Style",
-		price: "$90",
-		availability: true
+		price: "90",
+		availability: true,
+		images:[
+			"images/porter3.jpg",
+			"images/porter2.jpg",
+
+		]
 	},
 	{
 		brand: "A.P.C",
 		description : "Sweet French",
-		price: "$180",
-		availability: false
+		price: "180",
+		availability: false,
+		images:[
+			"images/porter2.jpg",
+			"images/porter4.jpg",
+
+		]
 	},
 	{
 		brand: "Acne Studios",
-		description : "Modern fuccboi clothing",
-		price: "$125",
-		availability: true
-	},
-	{
-		brand: "Saint Laurent",
-		description : "Top of the line stuff",
-		price: "$100",
-		availability: false
-	}]
+		description : "Modern Swedish silk",
+		price: "125",
+		availability: true,
+		images:[
+			"images/porter4.jpg",
+			"images/porter3.jpg",
 
+		]
+	}]
 })();
